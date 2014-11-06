2e1deb8d57f09601acdacd4fb1dad24caf3deb50
Gordon Hall <vladamirthebadamir@gmail.com>
2014-11-05 19:45:56 -0500

    Phuong helped Gordon fix the layouts
    Phuong helped to fix the left pane and make it scrollable

--- LongDungeon/gen/com/example/longdungeon/R.java
+++ LongDungeon/gen/com/example/longdungeon/R.java
@@ -1096,24 +1096,24 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050057;
+        public static final int action_settings=0x7f050058;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
-        public static final int attackTxt=0x7f05004f;
+        public static final int attackTxt=0x7f050050;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
-        public static final int defendTxt=0x7f050050;
+        public static final int defendTxt=0x7f050051;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
-        public static final int enemyBox=0x7f050042;
-        public static final int enemyHpLabel=0x7f050043;
-        public static final int enemyHpTxt=0x7f050044;
-        public static final int enemyName=0x7f050041;
+        public static final int enemyBox=0x7f050043;
+        public static final int enemyHpLabel=0x7f050044;
+        public static final int enemyHpTxt=0x7f050045;
+        public static final int enemyName=0x7f050042;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
         public static final int home=0x7f050014;
@@ -1121,35 +1121,36 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int imageView1=0x7f05003d;
-        public static final int imageView2=0x7f050054;
-        public static final int imageView3=0x7f050055;
-        public static final int imageView4=0x7f050056;
-        public static final int itemTxt=0x7f050052;
+        public static final int imageView1=0x7f05003e;
+        public static final int imageView2=0x7f050055;
+        public static final int imageView3=0x7f050056;
+        public static final int imageView4=0x7f050057;
+        public static final int itemTxt=0x7f050053;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
-        public static final int magicTxt=0x7f050051;
+        public static final int magicTxt=0x7f050052;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
-        public static final int playerHp=0x7f050046;
-        public static final int playerHpLbl=0x7f050047;
-        public static final int playerHpTxt=0x7f050048;
-        public static final int playerMgk=0x7f050049;
-        public static final int playerMgkLbl=0x7f05004a;
-        public static final int playerMgkTxt=0x7f05004b;
-        public static final int playerName=0x7f050045;
-        public static final int playerStm=0x7f05004c;
-        public static final int playerStmLbl=0x7f05004d;
-        public static final int playerStmTxt=0x7f05004e;
+        public static final int playerHp=0x7f050047;
+        public static final int playerHpLbl=0x7f050048;
+        public static final int playerHpTxt=0x7f050049;
+        public static final int playerMgk=0x7f05004a;
+        public static final int playerMgkLbl=0x7f05004b;
+        public static final int playerMgkTxt=0x7f05004c;
+        public static final int playerName=0x7f050046;
+        public static final int playerStm=0x7f05004d;
+        public static final int playerStmLbl=0x7f05004e;
+        public static final int playerStmTxt=0x7f05004f;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
-        public static final int runTxt=0x7f050053;
-        public static final int scoreBox=0x7f05003e;
-        public static final int scoreLabel=0x7f05003f;
-        public static final int scoreTxt=0x7f050040;
+        public static final int runTxt=0x7f050054;
+        public static final int scoreBox=0x7f05003f;
+        public static final int scoreLabel=0x7f050040;
+        public static final int scoreTxt=0x7f050041;
+        public static final int scrollView1=0x7f05003d;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1298,7 +1299,7 @@ containing a value of this type.
          */
         public static final int AppBaseTheme=0x7f0b008b;
         /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
+ something.. All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f0b008c;
         /**  Mimic text appearance in select_dialog_item.xml 
